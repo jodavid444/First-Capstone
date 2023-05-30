f87813d8d782efa5a6753616b1e3db1ada3674ed
Joseph David <jodavid444dave@gmail.com>
2023-05-30 04:19:45 +0100

    Resolve required changes and improvement from code reviewer manageer

---
+++ js/data.js
@@ -0,0 +1,48 @@
+const data = {
+  features: [
+    {
+      key: 0,
+      featureImage: './img/Crosby.0368d0d0.png',
+      featureName: 'Naill Crosby',
+      featureDescription: ' CEO / Founder',
+      featureMessage: 'Providing a library that supports multiple frameworks is a huge challenge, my team an I came up with a way for our grid component to support all frameworks.',
+    },
+    {
+      key: 1,
+      featureImage: './img/Hawkins.d3e858f0.png',
+      featureName: 'Tyler Hawkins',
+      featureDescription: 'Senior Software Engineer',
+      featureMessage: 'Test development is a programming paradigm in which you write your tests first and your source code. It is perfect when you’re writing code that has clear inputs',
+    },
+    {
+      key: 2,
+      featureImage: './img/Rakowski.317b77c2.png',
+      featureName: 'Nivetha Maran',
+      featureDescription: 'Software Engineer Pro',
+      featureMessage: 'Learn all about the react hooks, the main advantages of react hooks, different type of hooks, and how it can replace with redux in most of the applications.',
+    },
+    {
+      key: 3,
+      featureImage: './img/Kehl.6d83b400.png',
+      featureName: 'Roy Derks',
+      featureDescription: 'Full Stack Engineer',
+      featureMessage: 'TypeScript aids JavaScript applications. They make sure that your props are passed in the correct form and shape throughout your application.',
+  },
+  {
+      key: 4,
+      featureImage: './img/cang5.jpg',
+      featureName: 'Daniel Cutler',
+      featureDescription: 'Senior React Developer',
+      featureMessage: ' Because they build a relationship that is based on trust . Over they years they have proven that delivery is fast and stable',
+    },
+    {
+      key: 3,
+      featureImage: './img/istockphoto-1289834546-612x612.jpg',
+      featureName: 'Elizabeth Borg',
+      featureDescription: 'Full Stack Enginneer',
+      featureMessage: ' Because they build a relationship that is based on trust . Over they years they have proven that delivery is fast and stable',
+    },
+  ],
+};
+
+export default data;
